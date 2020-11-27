a26f40b87c31cc798c2ad3387dd10d8f2b04de31
Edwin Sam <edwin@edwins-mbp.lan>
2020-11-27 15:54:31 -0800

    username, pw, and button for login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
+import './style.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hi Edwin</h1>
-    </div>
+    <>
+      <div className="login">
+          <h1>Instagram</h1>
+          <input id="username" type="text" placeholder="Phone number, username, or email" />
+          <input id="password" type="text" placeholder="Password" />
+          <button>Log In</button>
+      </div>
+       
+    </>
   );
 }
 
